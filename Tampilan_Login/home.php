469f232e41be8094f6e79d324be7e62c8846195c
Samuel <samuel11lumbantoruan@gmail.com>
2025-11-14 17:33:30 +0700

    Update home.php

--- Tampilan_Login/home.php
+++ Tampilan_Login/home.php
@@ -30,7 +30,7 @@
           <a class="nav-link" href="menu.html">Menu</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="signup.html">Sign Up</a>
+          <a class="nav-link" href="signup.php">Sign Up</a>
         </li>
       </ul>
     </div>
