b9c23e7a609cb1126d4addb2196cbe6a1336be10
Samuel <samuel11lumbantoruan@gmail.com>
2025-11-15 17:34:54 +0700

    remove duplicated folder inside Tampilan_Login

--- Tampilan_Login/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();      // aktifkan session dulu
-session_destroy();    // hapus semua session (termasuk data login)
-header("Location: index.php"); // kembali ke halaman login
-exit;
-?>
